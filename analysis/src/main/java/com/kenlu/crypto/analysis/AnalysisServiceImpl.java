2398424fa2ff7fd8fcd7a1fe32fa5406d92bf934
luluuuluuuuu <a26331057@gmail.com>
2018-05-07 18:11:12 +0100

    Kmeans using PCA

--- analysis/src/main/java/com/kenlu/crypto/analysis/AnalysisServiceImpl.java
+++ analysis/src/main/java/com/kenlu/crypto/analysis/AnalysisServiceImpl.java
@@ -29,9 +29,9 @@ public class AnalysisServiceImpl implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-//        kMeansClusteringAnalysis.run();
         this.analyseCorrelation();
         this.analysePCA();
+        this.analyseKMeans();
     }
 
     public void analyseCorrelation() {
@@ -44,10 +44,18 @@ public class AnalysisServiceImpl implements CommandLineRunner {
 
     public void analysePCA() {
         today.set(Calendar.HOUR_OF_DAY, 2);
-        today.set(Calendar.MINUTE, 20);
+        today.set(Calendar.MINUTE, 15);
         today.set(Calendar.SECOND, 0);
         Date firstTime = today.getTime();
         timer.schedule(principalComponentAnalysis, firstTime, TimeUnit.MILLISECONDS.convert(1, TimeUnit.DAYS));
+    }
+
+    public void analyseKMeans() {
+        today.set(Calendar.HOUR_OF_DAY, 2);
+        today.set(Calendar.MINUTE, 20);
+        today.set(Calendar.SECOND, 0);
+        Date firstTime = today.getTime();
+        timer.schedule(kMeansClusteringAnalysis, firstTime, TimeUnit.MILLISECONDS.convert(1, TimeUnit.DAYS));
     }
 
 }
