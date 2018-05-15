33db5a303849cbee7a2cf926eaf5d03e0c10da27
luluuuluuuuu <a26331057@gmail.com>
2018-05-15 21:19:42 +0100

    LSTM

--- analysis/src/main/java/com/kenlu/crypto/analysis/supervised/lstm/LSTMAnalysis.java
+++ analysis/src/main/java/com/kenlu/crypto/analysis/supervised/lstm/LSTMAnalysis.java
@@ -1,4 +1,12 @@
 package com.kenlu.crypto.analysis.supervised.lstm;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class LSTMAnalysis {
+
+    public void run() {
+
+    }
+
 }
